2703d20c0613e5f3394860cc7b84f10159b0ef86
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-20 16:31:00 -0400

    final

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,37 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Music {
 
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int clicksDown =0;
+        int clicksup =0;
+        int loopclicks= 0;
+        for(int i = 0; i < playList.length; i++) {
+            if (selection.equals(playList[i])) {
+                if (startIndex < i) {
+                    clicksDown = i - startIndex;
+
+                } else if (i < startIndex) {
+                    clicksup = startIndex - i;
+                }
+            }
+            loopclicks = (startIndex) + ((playList.length) - i);
+        }
+        if (clicksup == loopclicks) {
+            return clicksup;
+        } else if (clicksDown == loopclicks)
+            return clicksDown;
+        return loopclicks;
     }
+
+
 }
